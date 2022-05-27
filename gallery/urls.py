9e2414a0f3a9170521d0cf507aa11aaba4608899
MugureNjunge <maureennjunge22@gmail.com>
2022-05-27 06:21:20 +0300

    create view function and url path to ensure my app is running

--- gallery/urls.py
+++ gallery/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from photoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # configured the url
+    path('',views.index, name="homepage")
 ]
+
