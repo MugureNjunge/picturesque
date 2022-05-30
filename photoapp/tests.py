bf91720abe8d735dc95803c8249f7a77fa40d94e
MugureNjunge <maureennjunge22@gmail.com>
2022-05-30 21:57:32 +0300

    run tests for my category model

--- photoapp/tests.py
+++ photoapp/tests.py
@@ -1,45 +1,11 @@
 from django.test import TestCase
-from .models import category, Location,Pin
+from .models import Image, Category, Location
 
 # Create your tests here.
-class Testcategory(TestCase):
+class TestCategory(TestCase):
     def setUp(self):
-        self.category_trial = category(category_name="category trial")
+        self.category_trial = Category(category_name="fashion")
 
     def test_category_instance(self):
-        self.assertTrue(isinstance(self.category_trial, category))
+        self.assertTrue(isinstance(self.category_trial, Category))
 
-class TestLocation(TestCase):
-    def setUp(self):
-        self.location_trial = Location(location="Cote d'ivoire")
-
-    def test_location_instance(self):
-        self.assertTrue(isinstance(self.location_trial, Location))
-
-class TestImages(TestCase):
-    def setUp(self):
-        self.category_trial = category(category_name="category trial")
-        self.category_trial.save()
-        
-        self.location_trial = Location(location="Cote d'ivoire")
-        self.location_trial.save()
-        
-        self.image_trial = Pin(pin_name="pin",pin_description="abcdef",category=self.category_trial,location=self.location_trial,pin_image='../media/images/bellaciao.jpg')
-        
-    def tearDown(self):
-        category.objects.all().delete()
-        Location.objects.all().delete()
-        Pin.objects.all().delete()
-
-    def test_image_instance(self):
-        self.assertTrue(isinstance(self.image_trial, Pin))
-        
-    def test_save_pins(self):
-        self.image_trial.save_pin()
-        all_images = Pin.objects.all()
-        self.assertTrue(len(all_images) == 1)
-        
-    def test_delete_image(self):
-        self.image_trial.save_pin()
-        self.image_trial.delete_image(self.image_trial.id)
-        self.assertTrue(len(Pin.objects.all()) == 0)
